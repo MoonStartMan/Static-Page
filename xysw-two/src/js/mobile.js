2701a5de64bd3ff4bae813b20d773097bdc110d8
WangXiao <39123418+MoonStartMan@users.noreply.github.com>
2019-11-07 19:00:34 +0800

    鑫悦商务二开网站

---
+++ xysw-two/src/js/mobile.js
@@ -0,0 +1,36 @@
+var data = [
+    {
+        src : "../images/mobile/1.png"
+    },
+    {
+        src : "../images/mobile/2.png"
+    },
+    {
+        src : "../images/mobile/3.png"
+    },
+    {
+        src : "../images/mobile/4.png"
+    },
+    {
+        src : "../images/mobile/5.png"
+    },
+    {
+        src : "../images/mobile/6.png"
+    },
+    {
+        src : "../images/mobile/7.png"
+    },
+]
+
+function createTem(template){
+    for(let i=0; i<template.length; i++) {
+        const templateItem = template[i];
+        $(".main").append(`
+        <div class="tab-box">
+            <img src=${templateItem.src} alt="">
+        </div>
+        `)
+    }
+}
+
+createTem(data);
